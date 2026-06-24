c1cc8581a6755b4fb23b9d8a6e096b66feae2406
chingumbe260 <149673737+chingumbe260@users.noreply.github.com>
2026-06-24 07:41:41 -1000

    Delete src/components/ui/button directory

--- src/components/ui/button/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Button, { type ButtonProps } from "@mui/material/Button";
-
-export type AppButtonProps = ButtonProps;
-
-export function AppButton(props: AppButtonProps) {
-  return <Button variant="contained" {...props} />;
-}
